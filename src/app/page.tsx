ab40a7c4438e78d3f95be0dd3f2bd0a78b75af1d
Rahul Pradhan <74361662+rahulp18@users.noreply.github.com>
2023-06-16 14:44:20 +0530

    button bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,1 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Button from "@/components/Button/Button";
-
-export default function Home() {
-  return (
-    <div className="">
-      <h1 className="">Hello From Rahul</h1>
-      <Button>Submit</Button>
-    </div>
-  );
-}
+export { default as Button } from "../components/Button/Button";
