5264924a958db4deeb8db19402f5ad33bce78f74
Rahul Pradhan <74361662+rahulp18@users.noreply.github.com>
2023-06-16 14:56:20 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,12 @@
-export { default as Button } from "../components/Button/Button";
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="">Hello From Rahul</h1>
+      <Button>Submit</Button>
+    </div>
+  );
+}
